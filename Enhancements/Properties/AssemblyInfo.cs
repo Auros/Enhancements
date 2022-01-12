a68d4c3f157eba80e3960b69506712a8d82d9ccd
Auros Nexus <41306347+Auros@users.noreply.github.com>
2022-01-12 13:43:34 -0500

    Update AssemblyInfo.cs

--- Enhancements/Properties/AssemblyInfo.cs
+++ Enhancements/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Enhancements")]
-[assembly: AssemblyCopyright("Copyright © Auros 2021")]
+[assembly: AssemblyCopyright("Copyright © Auros 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.11")]
-[assembly: AssemblyFileVersion("3.0.11")]
+[assembly: AssemblyVersion("3.0.12")]
+[assembly: AssemblyFileVersion("3.0.12")]
